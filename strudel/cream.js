f43f9e4df9cf871e9e515ea4ff6392805a1c03ed
Matt <39347872+Chaaronn@users.noreply.github.com>
2025-10-26 18:47:53 +0000

    Refactor piano and grace notes in cream.js

--- strudel/cream.js
+++ strudel/cream.js
@@ -2,11 +2,11 @@ setcps(91/60/4)
 samples('github:eddyflux/crate')
 
 let drums = stack(
-  s("bd ~ bd*2 <[~ bd] [bd bd]>").gain(0.9),
-  s("~ cp ~ cp").gain(0.8)
+  s("bd cp bd*2 <[~ bd] [bd bd]>").gain(0.9),
+  s("~ ~ ~ cp").gain(0.8)
 )._scope()
 
-let piano = note("fs5@1 gs5@0.75 a5@1 b5@1 cs6@1 fs6@1 e6@0.25 fs6@0.25 e6@0.25 ds6@1.25 cs6@1 b5@1 a5@1.25 gs5@0.75 fs5@1 fs5@1 fs5@2")
+let piano = note("fs5@1 fs5@0.25 gs5@0.75 a5@1 b5@1 cs6@1 fs6@1 e6@0.25 fs6@0.25 e6@0.25 ds6@1.25 cs6@1 b5@1 a5@1.25 gs5@0.75 fs5@1 fs5@1 fs5@2")
   .s("gm_piano")
   .slow(2)
   .sustain(2)
@@ -14,7 +14,6 @@ let piano = note("fs5@1 gs5@0.75 a5@1 b5@1 cs6@1 fs6@1 e6@0.25 fs6@0.25 e6@0.25 
 
 
 let root = note("cs3")
-  .off(1/8, x => x.note("a4, fs4"))
   .s("gm_piano")
   .struct("x ~ ~ ~")                 // hit on beat 1 each bar
   .sustain(0.95)
@@ -22,14 +21,16 @@ let root = note("cs3")
   .cut(1)                            // stop previous chord when retriggered
   .clip(3.75)
 
-let grace = note("[a5, fs5]")
+
+let grace = note("[a3, fs3]")
   .s("gm_piano")
-  .struct("~ x ~ ~")
-  .late(1/4)
+  .struct("x ~ ~ ~")
+  .late(1/64)
   .sustain(0.6)
   .gain(0.5)
 
 let piano2 = stack(root, grace)
 
-
-stack(drums, piano, piano2).pianoroll()
+$: piano.pianoroll()
+$: drums
+$: piano2
