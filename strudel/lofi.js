7ec5a64eb7c40703a68c5cdc26a539debe2f634c
Matt <39347872+Chaaronn@users.noreply.github.com>
2025-10-27 13:00:02 +0000

    Update chord and bass definitions in lofi.js

--- strudel/lofi.js
+++ strudel/lofi.js
@@ -1,17 +1,20 @@
 // "LOFI"
 
-let chords = chord("<Bbm9 Fm9>/2").dict('ireal')
+let chords = chord("<Dm7 G7>/2").dict('ireal')
 
 let background = chords.offset(-1).voicing().s("gm_epiano1:1")
   .phaser(4).room(.5)
 
-let bass = n("<<0!3 1*2> 2!3 3*2>").set(chords).mode("root:g2")
+let bass = n("<0 <1*2 [5 1]>>").set(chords).mode("root:g2")
   .voicing().s("gm_acoustic_bass")
   .gain(0.9)
 
-$: stack(background, bass)
+let melody = chords.n("[0 <~ 3 <2 1>>*2]")
+  .anchor("D5").voicing().segment(4).gain(1.5).s("gm_electric_guitar_muted:0").phaser(0).room(.75)
+
+$: stack(background, bass, melody)
 
 let drums = s("bd sd [bd bd] sd").bank("RolandTR808").gain(".7,.8")
-let hats = s("hh*8").bank("RolandTR808").gain(".3").decay(.1)
+let hats = s("<hh*8>").bank("RolandTR808").gain(".3").decay(.1)
 
 $: stack(drums, hats).gain(0.9).swing(4)._scope()
